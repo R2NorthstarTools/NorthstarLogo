d1e2904854aaa3495208e39606ea2892adf22151
laundmo <laurinschmidt2001@gmail.com>
2022-06-02 00:58:53 +0200

    Blend file for pride version

---
+++ Sources/logo/pride-version/convert_svg.bat
@@ -0,0 +1,12 @@
+inkscape -d 100 -D -o 00.png rainbow.svg
+inkscape -d 100 -D -o 01.png lesbian.svg
+inkscape -d 100 -D -o 02.png gay.svg
+inkscape -d 100 -D -o 03.png bi.svg
+inkscape -d 100 -D -o 04.png trans.svg
+inkscape -d 100 -D -o 05.png genderfluid.svg
+inkscape -d 100 -D -o 06.png nonbinary.svg
+inkscape -d 100 -D -o 07.png a-sexuality.svg
+inkscape -d 100 -D -o 08.png a-romantic.svg
+inkscape -d 100 -D -o 09.png a-gender.svg
+inkscape -d 100 -D -o 10.png pan.svg
+inkscape -d 100 -D -o 11.png inter.svg
